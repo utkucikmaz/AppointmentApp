135f9f5bfea04cce6952f54025bf1967e3f1c9bb
Utku Cikmaz <utkucikmaz@gmail.com>
2023-10-16 23:03:56 +0200

    google calendar api

--- my-project-client/src/App.jsx
+++ my-project-client/src/App.jsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <ToastContainer />
-        </>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- my-project-server/routes/index.routes.js
+++ my-project-server/routes/index.routes.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const { google } = require("googleapis");
+const { OAuth2Client } = require("google-auth-library");
+
+const CLIENT_ID =
+    "759266247632-j6nk02r1jr5b6l07bpeic4prdcn2fd7j.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URI = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:5173/",
+    "https://localhost:5173/",
+    "http://localhost:5005",
+    "https://localhost:5005",
+];
+
+const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline", // This allows you to get a refresh token
+    scope: ["https://www.googleapis.com/auth/calendar.readonly"], // Adjust the scope based on your needs
+});
+console.log("Authorize this app by visiting this URL:", authUrl);
 
 router.get("/", (req, res, next) => {
-  res.json("All good in here");
+    async function listCalendars() {
+        const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
+        const res = await calendar.calendarList.list();
+        console.log(res.data);
+    }
+
+    // Call the function to list calendars
+    listCalendars();
 });
 
 module.exports = router;
